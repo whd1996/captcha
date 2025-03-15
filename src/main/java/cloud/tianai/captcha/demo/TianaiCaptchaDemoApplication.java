24a9705a702ef6fb0a4d02bed9453b7baa0ea199
whd <1263969092@qq.com>
2025-03-15 11:42:21 +0800

    feat: 验证码

---
+++ src/main/java/cloud/tianai/captcha/demo/TianaiCaptchaDemoApplication.java
@@ -0,0 +1,18 @@
+package cloud.tianai.captcha.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@Slf4j
+@Controller
+@SpringBootApplication
+public class TianaiCaptchaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianaiCaptchaDemoApplication.class, args);
+    }
+
+}
